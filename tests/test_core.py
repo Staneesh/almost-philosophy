7bf42446c9d3e9d82b9a9476b804b1abe617285c
Staneesh <blkreasons@outlook.com>
2023-04-22 22:11:20 +0200

    Refactored HPI retrieval into `almop`

--- tests/test_core.py
+++ tests/test_core.py
@@ -15,6 +15,13 @@ class TestCoreAlmop(unittest.TestCase):
         """Simple test function"""
         self.assertEqual(almop.add_one(4), 5)
 
+    def test_get_hpi(self):
+        """
+        Tests if data from eurostat for HPI index change
+        is retrieved and filtered OK.
+        """
+        self.assertEqual(len(almop.get_hpi(["PL", "DE"]).columns), 2)
+
 
 if __name__ == "__main__":
     unittest.main()
