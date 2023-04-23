0c5d2f6de183663d6a347f5e8ab5f27be1bbeac9
Staneesh <blkreasons@outlook.com>
2023-04-23 21:37:35 +0200

    Finished implementing persistent dataset pulling

--- almop/core.py
+++ almop/core.py
@@ -3,6 +3,11 @@
 for analysing causal structures of time series data.
 """
 
+import tomli
+
+def parse_config(config_name: str):
+    with open(config_name, mode="rb") as fp:
+        return tomli.load(fp)
 
 def add_one(number):
     """
