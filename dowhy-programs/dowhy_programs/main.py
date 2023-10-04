38be79aa309675ce4ef37afc6ed558553d72d227
Staneesh <blkreasons@outlook.com>
2023-10-04 18:11:02 +0200

    dowhy-poetry-dump

---
+++ dowhy-programs/dowhy_programs/main.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+from dowhy import CausalModel
+import dowhy.datasets 
+
+def main():
+    data = dowhy.datasets.linear_dataset(beta=10,
+            num_common_causes=5,
+            num_instruments = 2,
+            num_effect_modifiers=1,
+            num_samples=5000, 
+            treatment_is_binary=True,
+            stddev_treatment_noise=10,
+            num_discrete_common_causes=1)
+    df = data["df"]
+    print( df.head() )
+
+
+
